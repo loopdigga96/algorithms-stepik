6e8f4527f407a69e46e7ac8c420952903ae302b4
loopdigga96 <tretyak.1996@gmail.com>
2019-10-06 20:30:06 +0300

    trying to solve this shit

--- solutions_python/part6/3.py
+++ solutions_python/part6/3.py
@@ -3,53 +3,80 @@ def swap(a, b):
     return a, b
 
 
-def compare(a, b):
-    if a <= b:
-        return True
-    else:
-        return False
-
-
-def partition(a, l, r):
+def partition(a, l, r, cmp_func):
     x = a[l]
     j = l
 
     for i in range(l+1, r+1):
-        if compare(a[i], x):
+        if cmp_func(a[i], x):
             j += 1
             a[j], a[i] = swap(a[j], a[i])
     a[l], a[j] = swap(a[l], a[j])
     return j
 
 
-def qsort(a, l, r):
-    if l < r:
-        m = partition(a, l, r)
-        qsort(a, l, m-1)
-        l = m + 1
-        # qsort(a, m+1, r)
+def qsort(a, l, r, cmp_fun):
+    if l >= r:
+        return
+    m = partition(a, l, r, cmp_fun)
+    qsort(a, l, m-1, cmp_fun)
+    qsort(a, m+1, r, cmp_fun)
 
 
 if __name__ == "__main__":
-    import random
+    from copy import deepcopy
+    from functools import cmp_to_key
     # n, m = input().split()
     a = []
-    lines = [[0, 5], [7, 10]]
-    points = [1, 6, 11]
-    # points = [random.randrange(0, 10**3) for _ in range(10**4)]
-    # for _ in range(int(n)):
+
+    # lines = [[0, 5],
+    #          [7, 10]]
+    # points = [1, 6, 11]
+    lines = [[0, 3],
+             [1, 3],
+             [2, 3],
+             [3, 4],
+             [3, 5],
+             [3, 6]]
+    points = [1, 2, 3, 4, 5, 6]
+
+    LEFT = -1
+    POINT = 0
+    RIGHT = 1
     for st, en in lines:
-        # st, en = tuple(map(int, input().split()))
-        a.extend(((st, 'start'), (en, 'end')))
+    # for _ in range(int(n)):
+    #     st, en = tuple(map(int, input().split()))
+        a.extend(((st, LEFT, -1), (en, RIGHT, -1)))
+    #     a.append((st, en))
 
-    for p in points:
-        a.append((p, 'point'))
+    # points = list(map(int, input().split()))
+    for idx, p in enumerate(points):
+    #      a.append((p, 'point'))
+        a.append((p, POINT, idx))
+
+    def sort_logic(a, b):
+        if a[0] != b[0]:
+            return a[0] < b[0]
+        else:
+            return a[1] < b[1]
+
+    qsort(a, 0, len(a) - 1, lambda a, b: sort_logic(a, b))
     print(a)
+    print(points)
+
+    cur = 0
+    answer = list(range(len(points)))
+    for obj in a:
+        if obj[1] == LEFT:
+            cur += 1
+        elif obj[1] == RIGHT:
+            cur -= 1
+        elif obj[1] == POINT:
+            idx = obj[2]
+            answer[idx] = cur
+        else:
+            pass
 
 
-
-    #a = [9, 4, 5, 2, 6, -1, 100, 0, 0, 0]
-    #a = [0,1,2,3,4,5,6,7,8,9]
-    qsort(a, 0, len(a) - 1)
-    a[0], a[1] = swap(a[0], a[1])
-    print(a)
+    print(*answer)
+    # https://www.youtube.com/watch?v=jvRWnf4q9C8
