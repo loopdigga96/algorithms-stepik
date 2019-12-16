957732e22a825dfb0cb7055af2ac5a3d429b94e5
loopdigga96 <tretyak.1996@gmail.com>
2019-12-16 22:15:48 +0300

    tree height prototype;

---
+++ structures/basic_data_structures/tree_height.py
@@ -0,0 +1,12 @@
+def find(root_idx, tree):
+    # this method iterates through all children and call recursive function that returns height
+    # height = 1
+    # for c in children:
+    # height = max(height, find(children, tree)
+    # return height
+    pass
+
+
+if __name__ == '__main__':
+    n = input()
+    nodes = list(map(int, input().split()))
