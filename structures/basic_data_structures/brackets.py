48fa4d4bbb3c651b968f6142928b114dde977bad
loopdigga96 <tretyak.1996@gmail.com>
2019-12-16 17:11:21 +0300

    brackets task

--- structures/basic_data_structures/brackets.py
+++ structures/basic_data_structures/brackets.py
@@ -1,27 +1,48 @@
+"""
+Формат входа.
+Строка s[1 . . . n], состоящая из заглавных и прописных букв латинского алфавита, цифр,
+знаков препинания и скобок из множества []{}().
+Формат выхода.
+Если скобки в s расставлены правильно, выведите строку “Success".
+В противном случае выведите индекс (используя индексацию с единицы) первой закрывающей скобки, для которой нет
+соответствующей открывающей. Если такой нет, выведите индекс первой открывающей скобки, для которой нет
+соответствующей закрывающей.
+
+"""
+
+
 def check_brackets(brackets):
     opening = ['[', '(', '{']
     closing = [']', ')', '}']
+    mapping = dict(zip(opening, closing))
     stack = []
+    indexes = []
 
-    for b in brackets:
+    for idx, b in enumerate(brackets):
         if b in opening:
             stack.append(b)
-        else:
+            indexes.append(idx + 1)
+        elif b in closing:
             if len(stack) == 0:
-                return False
-            top = stack.pop()
+                return idx + 1
+            else:
+                top = stack.pop()
+                last_index = indexes.pop()
 
-            if any([top == pair[0] and b != pair[1] for pair in zip(opening, closing)]):
-                return False
+            if mapping[top] != b:
+                return idx + 1
 
-    return True
+    if len(stack) == 0:
+        return 0
+    else:
+        return indexes.pop()
 
 
 if __name__ == '__main__':
     brackets = input()
     result = check_brackets(brackets)
 
-    if result:
+    if result == 0:
         print('Success')
     else:
-        print(len(brackets))
+        print(result)
