df38b5416240d51ae9b14cc4550e8d2a960e95fc
loopdigga96 <tretyak.1996@gmail.com>
2019-12-17 13:15:45 +0300

    update algorithm, need to be optimized

--- structures/basic_data_structures/tree_height.py
+++ structures/basic_data_structures/tree_height.py
@@ -1,12 +1,42 @@
-def find(root_idx, tree):
-    # this method iterates through all children and call recursive function that returns height
-    # height = 1
-    # for c in children:
-    # height = max(height, find(children, tree)
-    # return height
-    pass
+import sys
+
+sys.setrecursionlimit(20000)
+
+
+def find(child, tree, memory):
+    height = 1
+    if child not in memory:
+        memory[child] = height
+        return height
+    elif memory[child] != -1:
+        return memory[child]
+    else:
+        # TODO: optimize this loop
+        for c, p in enumerate(tree):
+            if tree[c] == child:
+                if c in memory and memory[c] != -1:
+                    compare_height = memory[c] + 1
+                else:
+                    compare_height = find(c, tree, memory) + 1
+                height = max(height, compare_height)
+        memory[child] = height
+        return height
 
 
 if __name__ == '__main__':
     n = input()
-    nodes = list(map(int, input().split()))
+    nodes = []
+    memory = {}
+    root = None
+
+    for idx, node in enumerate(map(int, input().split())):
+        nodes.append(node)
+        memory[node] = -1
+
+        if node == -1:
+            root = idx
+
+    for c, p in enumerate(nodes):
+        height = find(c, nodes, memory)
+
+    print(memory[root])
